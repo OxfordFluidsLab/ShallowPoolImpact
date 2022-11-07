1ca11362b74cf4677af5bcd09e811633cbe412f0
Thomas Sykes <t.c.sykes@outlook.com>
2022-11-07 12:15:33 +0000

    Initial commit of figure 3 data

---
+++ Raw_figure_data/fig3_plot.m
@@ -0,0 +1,55 @@
+% fig3_plot.m
+%
+% Script that can be used to gain a basic plot for the raw data underlying
+% figure 3 (fig3.csv)
+%
+% Source:  https://github.com/OxfordFluidsLab/ShallowPoolImpact
+% Licence: GPL-3.0 (see the Git repo).
+%
+% T.C. Sykes (t.c.sykes@outlook.com)
+% University of Oxford (2022)
+
+% Read in the data from a csv file
+t = readtable('fig3.csv');
+
+% Setup figure
+clf
+hold on
+
+% Loop over each row in the csv file
+for ii = 1:size(t,1)
+    
+    % Determine the desired marker colour from the outcome given
+    switch t.outcome{ii}
+        case 'lamella'
+            col = 'r';
+        case 'separate'
+            col = 'g';
+        case 'axisymmetric'
+            col = 'y';
+        otherwise
+            continue
+    end
+    
+    % Determine the desired marker type from whether satellites are present
+    switch t.satellites(ii)
+        case true
+            mkr = 'o';
+        case false
+            mkr = 'v';
+        otherwise
+            continue
+    end
+    
+    % Plot the point with the given marker and colour
+    scatter(t{ii,1},t{ii,2},20,col, 'filled', 'Marker',mkr);
+    
+end
+
+% Figure formatting
+box on
+set(gca,'xscale','log');            % Select log scale for the 
+xlim([min(t.hstar),max(t.hstar)])   % Limit x axis to the data
+ylim([min(t.We),max(t.We)])         % Limit y axis to the data
+xlabel('hstar')                     % Label x axis
+ylabel('We')                        % Label y axis
